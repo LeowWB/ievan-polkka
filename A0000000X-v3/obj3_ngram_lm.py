f325c209caadebba212f220788992dfab6282eae
LeowWB <41581512+LeowWB@users.noreply.github.com>
2021-02-02 23:57:25 +0800

    comments regarding stuff on slack mentioned by prof in obj3; also impl,ement read_file

--- A0000000X-v3/obj3_ngram_lm.py
+++ A0000000X-v3/obj3_ngram_lm.py
@@ -2,6 +2,25 @@
     NUS CS4248 Assignment 1 - Objective 3 (n-gram Language Model)
 
     Class NgramLM for handling Objective 3
+
+
+    Please use the following command for objective 3:
+    OBJ3 path=STRING smooth=TYPE n_gram=INT k=FLOAT text=STRING next_word=STRING length=INT
+    text: for generate_word(text), generate_next_word_probability(text, word), perplexity(text).
+    next_word: for generate_next_word_probability(text, word)
+    length: for generate_text(length)
+    Please update the pattern_dict  accordingly.
+    In the outputs, please mention which function gives what output. For instance,
+    """
+    Generated word: OUTPUT
+    Probability of next word: OUTPUT
+    Perplexity: OUTPUT
+    Generated text: OUTPUT
+    """ 
+    If you implement any other type of smoothing, please mention in the write-up as to how run it
+    as well - what is smooth=TYPE  value, does it require extra arguments like lambda, etc.?
+
+
 '''
 import random, math
 
@@ -17,41 +36,51 @@ class NgramLM(object):
 
         '''
         # Initialise other variables as necessary
-        # TODO Write your code here
         self.n = n
         self.k = k
         self.word_count_dict = {}
+        self.vocabulary = set()
+        self.ngrams = []
 
     def update_corpus(self, text):
-        ''' Updates the n-grams corpus based on text '''
-        # TODO Write your code here
+        ''' Updates the n-grams corpus based on text. ADD to corpus'''
+        
         pass
+
 
     def read_file(self, path):
         ''' Read the file and update the corpus  '''
-        # TODO Write your code here
-        pass
+        with open(path, encoding='utf-8', errors='ignore') as f:
+            text = f.read()
+        text = text.lower()
+        self.update_corpus(text)
+
 
     def ngrams(self):
         ''' Returns ngrams of the text as list of pairs - [(sequence context, word)] '''
         # TODO Write your code here
         pass
 
+
     def add_padding(self):
         '''  Returns padded text '''
         # TODO Write your code here
-        # Use '~' as your padding symbol
+        # Use '~' as your padding symbol just put it infront. don't pad end.
+        # pad each sentence of the corpus.
         pass
+
 
     def get_vocabulary(self):
         ''' Returns the vocabulary as set of words '''
         # TODO Write your code here
         pass
 
+
     def get_next_word_probability(self, text, word):
         ''' Returns the probability of word appearing after specified text '''
         # TODO Write your code here
         pass
+
 
     def generate_word(self, text):
         '''
@@ -61,10 +90,13 @@ class NgramLM(object):
         # TODO Write your code here
         pass
 
+
     def generate_text(self, length):
-        ''' Returns text of the specified length based on the learned model '''
+        ''' Returns text of the specified number of words based on the learned model '''
+        # MUST BE RANDOM> NOT DETERMINISTIC.
         # TODO Write your code here
         pass
+
 
     def perplexity(self, text):
         '''
