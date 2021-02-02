152ad9dd76c199282a35b3f886c1483789d09af9
LeowWB <41581512+LeowWB@users.noreply.github.com>
2021-02-02 21:33:52 +0800

    makeshift tests for obj2

---
+++ A0000000X-v3/to2.py
@@ -0,0 +1,44 @@
+# makeshift test code for obj2
+
+from obj2_weather import Weather
+
+EXPECTED_DEFAULT_QUERIES = [
+    '',
+    'hey',
+    'weather',
+    'weather rain',
+    'weather singapore',
+    'singapore weather',
+    'weather in singapore',
+    'the weather in singapore',
+    "singapore's weather",
+    'singapore weather?',
+    'weather in singapore?',
+    'what is the temperature like in singapore!'
+]
+
+EXPECTED_SINGAPORE_QUERIES = [
+    'is it going to be a rainy day in singapore',
+    "how's the weather in singapore",
+    'please tell me about the singapore weather',
+    'what is the temperature like in singapore?',
+    'is the singapore weather as hot as cairo or as rainy as london?'
+]
+
+w = Weather('')
+fail = False
+
+for x in EXPECTED_DEFAULT_QUERIES:
+    if w.weather(x) != Weather.DEFAULT:
+        print(x)
+        print(w.weather(x))
+        fail = True
+
+for x in EXPECTED_SINGAPORE_QUERIES:
+    if w.weather(x) != Weather.SINGAPORE_WEATHER:
+        print(x)
+        print(w.weather(x))
+        fail = True
+
+if not fail:
+    print('nice')
