d546f3e85597e9bf59f571c2a4834bf79370cc82
LeowWB <41581512+LeowWB@users.noreply.github.com>
2021-02-03 22:22:14 +0800

    add some test cases

--- A0000000X-v3/to3.py
+++ A0000000X-v3/to3.py
@@ -4,6 +4,12 @@ from pdb import set_trace
 TEXT = ("I am a dwarf and I'm digging a hole! Diggy diggy hole! Digging a hole!\n" +
         "DWARF! HOLE! DIGGY DIGGY HOLE! DIGGY DIGGY HOLE! DIGGY DIGGY HOLE!")
 
-ng = NgramLM(2, 0)
+ng = NgramLM(3, 0)
 ng.update_corpus(TEXT)
+
+assert ng.get_next_word_probability("i am a ", "dwarf") == 1
+assert ng.get_next_word_probability("i are a ", "dwarf") == 1/3
+assert ng.get_next_word_probability("hi","dwarf") == 2/26
+assert ng.get_next_word_probability("i am a", "miner") == 0
+
 set_trace()
