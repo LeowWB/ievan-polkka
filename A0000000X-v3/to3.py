74fbefd9027658b8121f4d37b66dbbe8113149f5
LeowWB <41581512+LeowWB@users.noreply.github.com>
2021-02-03 18:07:55 +0800

    change test case to be easier on the eyes

--- A0000000X-v3/to3.py
+++ A0000000X-v3/to3.py
@@ -1,10 +1,8 @@
 from obj3_ngram_lm import NgramLM
 from pdb import set_trace
 
-TEXT = ("I am a dwarf and I'm digging a hole! Diggy diggy hole! Digging a hole! I am a dwarf and " +
-        "I'm digging a hole! Diggy diggy hole! Digging a hole!\nDWARF! HOLE! DIGGY DIGGY HOLE! " +
-        "DIGGY DIGGY HOLE! DIGGY DIGGY HOLE! DWARF! HOLE! DIGGY DIGGY HOLE! DIGGY DIGGY HOLE! " +
-        "DIGGING A HOLE!")
+TEXT = ("I am a dwarf and I'm digging a hole! Diggy diggy hole! Digging a hole!\n" +
+        "DWARF! HOLE! DIGGY DIGGY HOLE! DIGGY DIGGY HOLE! DIGGY DIGGY HOLE!")
 
 ng = NgramLM(2, 0)
 ng.update_corpus(TEXT)
